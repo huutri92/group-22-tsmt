5e2275154f68accd6f00ee034464577d017df469
tuannd60358 <tuannd60358@fpt.edu.vn>
2014-03-06 08:22:42 +0000

    update

--- trunk/Project/TSMT/TSMT/Controllers/AdminController.cs
+++ trunk/Project/TSMT/TSMT/Controllers/AdminController.cs
@@ -30,6 +30,56 @@ namespace TSMT.Controllers
             model.AddUniversityExam(model.UniversityId, model.ExaminationId);
             return RedirectToAction("ManageUniversityExam", model);
         }
+	
 
+	 public ActionResult ManagerCategory()
+        {
+            DataAccess da = new DataAccess();
+            List<Category> list = da.SelectCategories();
+            return View(list);
+        }
+        public ActionResult AddNewCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNewCategory(Category cat)
+        {
+            DataAccess da = new DataAccess();
+            if (ModelState.IsValid)
+            {
+                int result = da.InsertCategory(cat);
+                return RedirectToAction("ManagerCategory");
+
+            }
+            return View(cat);
+        }
+        public ActionResult DeleteCategory(int id)
+        {
+            DataAccess da = new DataAccess();
+            int result = da.DeleteCategory(id);
+            return RedirectToAction("ManagerCategory");
+        }
+
+        //public ActionResult EditCategory()
+        //{
+            
+
+        //    return RedirectToAction("ManagerCategory");
+        //}
+        public ActionResult EditCategory(int id)
+        {
+            var _charityCateogory = new DataAccess();
+            _charityCateogory.GetCategoryInfo(id);
+            return View("EditCategory", _charityCateogory);
+        }
+
+        [HttpPost]
+        public ActionResult EditCategoryInfo(DataAccess model)
+        {
+            model.EditCategory(model);
+            return RedirectToAction("ManagerCategory", model);
+        }
     }
 }
---
+++ trunk/Project/TSMT/TSMT/Controllers/SponsorController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TSMT.Models;
+namespace TSMT.Controllers
+{
+    public class SponsorController : Controller
+    {
+        //
+        // GET: /Sponsor/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ManagerFund()
+        {
+            DataAccess da = new DataAccess();
+            List<Fund> list = da.SelectFunds();
+            return View(list);
+        }
+        public ActionResult AddNewFund()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNewFund(Fund fund)
+        {
+            DataAccess da = new DataAccess();
+            if (ModelState.IsValid)
+            {
+                int result = da.InsertFund(fund);
+                return RedirectToAction("ManagerFund");
+
+            }
+            return View(fund);
+        }
+        public ActionResult DeleteFund(int id)
+        {
+            DataAccess da = new DataAccess();
+            int result = da.DeleteFund(id);
+            return RedirectToAction("ManagerFund");
+        }
+
+        //public ActionResult EditFund(int id)
+        //{
+        //    var _charityFund = new DataAccess();
+        //    _charityFund.GetFundInfo(id);
+        //    return View("EditFund", _charityFund);
+        //}
+        
+        public ActionResult ManagerCar()
+        {
+            CarDBModel da = new CarDBModel();
+            List<Car> list = da.SelectCar();
+            return View(list);
+        }
+        public ActionResult AddNewCar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNewCar(Car car)
+        {
+            CarDBModel da = new CarDBModel();
+            if (ModelState.IsValid)
+            {
+                int result = da.InsertCar(car);
+                return RedirectToAction("ManagerCar");
+
+            }
+            return View(car);
+        }
+        public ActionResult DeleteCar(int id)
+        {
+            CarDBModel da = new CarDBModel();
+            int result = da.DeleteCar(id);
+            return RedirectToAction("ManagerCar");
+        }
+    }
+}
