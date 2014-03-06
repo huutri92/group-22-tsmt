28c2a42a7ac91890a2fa780d4111be54ed922712
vinhtnk60325 <vinhtnk60325@fpt.edu.vn>
2014-03-06 13:02:56 +0000

    add model

---
+++ trunk/Project/TSMT/TSMT/Models/CharityExamModel.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TSMT.Models
+{
+    public class NewCharity
+    {
+        public int CharityId { get; set; }
+        public int AccountId { get; set; }
+        public string FullName { get; set; }
+    }
+
+    public class CharityExamModel
+    {
+        readonly TSMTEntities db = new TSMTEntities();
+        public ChairitiesExam ChairitiesExam { get; set; }
+        public List<ChairitiesExam> ListVenuesChairitiesExams { get; set; }
+        public int CharityExamId { get; set; }
+        public int ExamId { get; set; }
+        public int CharityId { get; set; }
+        public int TotalSlotsLodges { get; set; }
+        public int AvailableSlotsLodges { get; set; }
+        public int TotalSlotsVehicles { get; set; }
+        public int AvailableSlotsVehicles { get; set; }
+        private string _fullName;
+        public Examination Examination { get; set; }
+
+        public List<Examination> ListExamination { get; set; }
+        public Charity Charity { get; set; }
+
+        public List<NewCharity> ListCharities { get; set; }
+
+        public string FullName
+        {
+            get { return "Hello"; }
+            set { _fullName = value; }
+        }
+
+        public void GetListExam()
+        {
+            ListExamination = new List<Examination>();
+            ListExamination = db.Examinations.ToList();
+        }
+
+        public void GetListCharity()
+        {
+            ListCharities = new List<NewCharity>();
+            ListCharities = (from c in db.Charities
+                            select new NewCharity
+                            {
+                                CharityId = c.CharityID,
+                                AccountId = c.AccountID,
+                                FullName = c.Account.Profile.Lastname + " " + c.Account.Profile.Firstname
+                            }).ToList<NewCharity>();
+        }
+
+
+        public List<ChairitiesExam> SelectVenuesChairitiesExams()
+        {
+            var list = from c in db.ChairitiesExams select c;
+            return list.ToList();
+        }
+
+        public int DeleteCharityExam(int id)
+        {
+            var ce = db.ChairitiesExams.FirstOrDefault(c => c.CharityExamID == id);
+            db.ChairitiesExams.Remove(ce);
+            int r = db.SaveChanges();
+            return r;
+        }
+        public bool AddCharityExam(ChairitiesExam ce)
+        {
+            try
+            {
+                using (var context = new TSMTEntities())
+                {
+                    context.ChairitiesExams.Add(ce);
+                    context.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        public void GetCharityExam(int ceId)
+        {
+            ChairitiesExam = new ChairitiesExam();
+            ChairitiesExam = db.ChairitiesExams.Find(ceId);
+        }
+
+        public void UpdateCharityExam(ChairitiesExam venue, int ceId)
+        {
+            var oldVenue = db.ChairitiesExams.Find(ceId);
+            oldVenue.ExamID = venue.ExamID;
+            oldVenue.CharityID = venue.CharityID;
+            oldVenue.TotalSlotsLodges = venue.TotalSlotsLodges;
+            oldVenue.AvailableSlotsLodges = venue.AvailableSlotsLodges;
+            oldVenue.TotalSlotsVehicles = venue.TotalSlotsVehicles;
+            oldVenue.AvailableSlotsVehicles = venue.AvailableSlotsVehicles;
+            db.SaveChanges();
+
+        }
+    }
+}
---
+++ trunk/Project/TSMT/TSMT/Models/ManageVenueModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data;
+using System.Web.Mvc;
+
+namespace TSMT.Models
+{
+    public class ManageVenueModel
+    {
+        readonly TSMTEntities db = new TSMTEntities();
+        public Venue Venue { get; set; }
+        public List<Venue> ListVenues { get; set; }
+        public int VenueId { get; set; }
+        public IEnumerable<SelectListItem> UniExamId { get; set; }
+        public String District { get; set; }
+        public String Name { get; set; }
+        public String Address { get; set; }
+
+        public List<Venue> SelectVenues()
+        {
+            var list = from c in db.Venues select c;
+            return list.ToList();
+        }
+
+        public int DeleteVenue(int id)
+        {
+            var ex = db.Venues.FirstOrDefault(c => c.VenueID == id);
+            db.Venues.Remove(ex);
+            int r = db.SaveChanges();
+            return r;
+        }
+        public bool AddVenue(Venue ven)
+        {
+            try
+            {
+                using (var context = new TSMTEntities())
+                {
+                    context.Venues.Add(ven);
+                    context.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        public void GetVenue(int venueId)
+        {
+            Venue = new Venue();
+            Venue = db.Venues.Find(venueId);
+        }
+
+        public void UpdateVenue(Venue venue, int venueId)
+        {
+            var oldVenue = db.Venues.Find(venueId);
+            oldVenue.Address = venue.Address;
+            oldVenue.District = venue.District;
+            oldVenue.Name = venue.Name;
+            oldVenue.UniExamID = venue.UniExamID;
+            db.SaveChanges();
+
+        }
+    
+    }
+}
