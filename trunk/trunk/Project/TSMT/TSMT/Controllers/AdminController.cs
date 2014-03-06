98d356a3e0acad921168eea400fe7da41c3a3e1d
hiennv60339@fpt.edu.vn <hiennv60339@fpt.edu.vn@699ba6e3-bb28-3011-eaa0-e077a6cf610d>
2014-03-06 07:53:16 +0000

    Nguyễn Vinh Hiển bàn giao trách nhiệm cho leader Nguyễn Duy Khoa

---
+++ trunk/trunk/Project/TSMT/TSMT/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TSMT.Models;
+
+namespace TSMT.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View("ControlPanelPage");
+        }
+
+        public ActionResult ManageUniversityExam()
+        {
+            var _adminUniExam = new AdminUniExaModel();
+            _adminUniExam.GetUniversityAndExam();
+            return View("AddUniverExam",_adminUniExam);
+        }
+
+        [HttpPost]
+        public ActionResult AddUniverExam(AdminUniExaModel model)
+        {
+            model.AddUniversityExam(model.UniversityId, model.ExaminationId);
+            return RedirectToAction("ManageUniversityExam", model);
+        }
+
+    }
+}
---
+++ trunk/trunk/Project/TSMT/TSMT/Controllers/CharityController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using TSMT.Models;
+using System.Web.Mvc;
+
+namespace TSMT.Controllers
+{
+    public class CharityController : Controller
+    {
+        //
+        // GET: /Charity/
+
+        public ActionResult ManageCar()
+        {
+            var _charityCar = new CharityCarModel();
+            _charityCar.GetCarsList(2);
+            return View("CarManagement", _charityCar);
+        }
+
+        public ActionResult ManageVolunteer()
+        {
+            var _charityVolunteer = new CharityVolunteerModel();
+            _charityVolunteer.GetListVolunteer(2);
+            return View("VolunteersManagement", _charityVolunteer);
+        }
+
+        public ActionResult AddCar()
+        {
+            var _charityCar = new CharityCarModel();
+            _charityCar.CharityExamId = 2;
+            return View("AddCar", _charityCar);
+        }
+
+        [HttpPost]
+        public ActionResult AddCar(CharityCarModel model)
+        {
+            //model.AddNewCar(model.NumberPlate, model.TotalSlot, model.DriverName, model.DriverPhone, model.CharityExamId);
+            model.AddNewCar(model);
+            //return View("CarManagement");
+           return RedirectToAction("ManageCar");
+
+        }
+
+        public ActionResult EditCar(int carId)
+        {
+            var _charityCar = new CharityCarModel();
+            _charityCar.GetCarInfo(carId);
+            return View("EditCar", _charityCar);
+        }
+
+        [HttpPost]
+        public ActionResult EditCarInfo(CharityCarModel model, int carId)
+        {
+            model.EditCar(model.Car, model.CarId);
+            return RedirectToAction("ManageCar");
+        }
+
+        public ActionResult DeleteCar(int carId)
+        {
+            var _charityCar = new CharityCarModel();
+            _charityCar.DeleteCar(carId);
+            return RedirectToAction("ManageCar");
+        }
+
+        public ActionResult RemoveVolunteer(int volunteerId)
+        {
+            var _charityVolunteer = new CharityVolunteerModel();
+            _charityVolunteer.RemoveVolunteer(volunteerId);
+            return RedirectToAction("ManageVolunteer");
+        }
+        
+}
+}
